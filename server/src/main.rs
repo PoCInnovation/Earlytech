98192afa6b5210795e9ed6c7da67e4f783fba844
Antoine0703 <antoine.beal@epitech.eu>
2026-02-03 19:19:10 +0100

    feat(serveur): add base code for rust server

---
+++ server/src/main.rs
@@ -0,0 +1,18 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(|| async { "Route principale" }))
+        .route("/ping", get(|| async { "pong" }));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    
+    println!("Server sur http://localhost:3000");
+
+    axum::serve(listener, app)
+        .await
+        .unwrap();
+}
